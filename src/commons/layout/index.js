1c90d4cda5b62eed670b4b1b307ca7db0f8308ed
MUYEONKIM <114132468+MUYEONKIM@users.noreply.github.com>
2023-06-14 17:52:09 +0900

    Add files via upload

---
+++ src/commons/layout/index.js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import LayoutHeader from "./header";
+import LayoutNavigation from "./navigation";
+import Sidebar from "./sidebar";
+import styled from "@emotion/styled";
+
+// 변하지 않는 값은 밖에다가 써줌
+const Main = [
+    "/",
+]
+
+const layoutFunction = [
+    "/login",
+]
+
+
+const LayoutDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+
+export default function Layout(props) {
+
+    const router = useRouter()
+
+    const isMain = Main.includes(router.asPath);
+    const isFunction = layoutFunction.includes(router.asPath);
+    return (
+        <LayoutDiv>
+            {!isMain && <Sidebar />}
+            <div>
+                {!isMain && <LayoutHeader />}
+                {!isFunction && <LayoutNavigation />}
+                <div style = {{display: "flex"}}>
+                    <div style={{width: "70%"}}>{props.children}</div>
+                </div>
+            </div>
+        </LayoutDiv>
+    )
+}
+
+
